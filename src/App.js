f8e77b8592bdd605deb89a42bb37166584bc01d2
harshika <harshikaagrawal852@gmail.com>
2025-09-14 18:22:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
 import RecipeList from "./components/RecipeList";
